4610f8ab4deff08ca41bdf0050f598e05e0f4870
Cristopher Peraza <cristopher.peraza@uabc.edu.mx>
2018-02-08 12:24:47 -0800

    Practica Angular
    Creacion de componentes, propiedades y tipo de datosa, constructor y metodos, diferencia entre var y let, creacion de clases y objetos.

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleados} from './empleados';//Importamos clase Empleados.
 
 @Component({
   selector: 'app-empleados',
   templateUrl: './empleados.component.html',
   styleUrls: ['./empleados.component.css'],
-  template: `
-    <h2>
-   {{ titulo_empleados }}!
-    </h2>
 
-    `
 })
 export class EmpleadosComponent  {
-    public titulo_empleados = 'EMPLEADOS';
+     titulo_empleados = 'COMPONENTE-EMPLEADOS';
+
+     //Objeto instalcia de la clase Empleados
+     public empleados:Empleados;
+     public trabajadores:Array<Empleados>;
+
+     constructor(){
+//Definimos lo valores que llevaran nuestros objetos de la clase Empleados(empleados.ts)
+       this.empleados = new Empleados( "Primer empleado",30,"Chofer",true);
+       this.trabajadores = [
+         new Empleados( "Primer trabajador",29,"Programador",true),
+         new Empleados( "segundo trabajador",35,"Diseñador",true)
+       ];
+     }
 
 
+
+
+     ngOnInit(){
+
+       console.log(this.empleados);
+       console.log(this.trabajadores);
+
+       }
 }
